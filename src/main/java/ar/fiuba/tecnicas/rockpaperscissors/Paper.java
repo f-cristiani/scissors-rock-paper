db6108bed7721008c3b46d0299531963926dacd4
Fernando Cristiani <fernandofcristiani@gmail.com>
2015-03-17 00:02:21 -0300

    Crea PaperTest y hace pasar paperBeatsRock()

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Paper.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Paper.java
@@ -2,4 +2,8 @@ package ar.fiuba.tecnicas.rockpaperscissors;
 
 public class Paper {
 
+	public Paper vs(Rock rock) {
+		return this;
+	}
+
 }
---
+++ src/test/java/ar/fiuba/tecnicas/rockpaperscissors/PaperTest.java
@@ -0,0 +1,16 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PaperTest {
+	
+	private Paper paper = new Paper();
+	private Rock rock = new Rock();
+
+    @Test
+    public void paperBeatsRock() {
+        assertEquals(paper, paper.vs(rock));
+    }
+}
